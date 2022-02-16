bd9d2b80021982847fdf29ab212bc4d82f2b01c5
ruheecharolia <98358771+ruheecharolia@users.noreply.github.com>
2022-02-16 13:56:26 -0600

    Update python3.py

--- python3.py
+++ python3.py
@@ -1,1 +1,14 @@
-from datetime import datetime
+with open("http_access_log.txt") as file_in:
+  array = []
+  for line in file_in:
+    array.append(line)
+    
+count = 0
+
+for i in array:
+  if i.find("Apr") != -1 and i.find("1995") != -1 and i.find("[11") != -1:
+    count += 1
+  elif i.find("May") != -1 and i.find("1995") != -1:
+    count += 1
+  elif i.find("Jun") != -1 and i.find("1995") !=-1:
+    count += 1
